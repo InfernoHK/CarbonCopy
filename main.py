ff631158ea4ae34f982989a3fdb0b1e373e4c41f
Dante Azucar <skynet8831@gmail.com>
2024-04-11 20:08:33 +0000

    im done with this

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def menu(choices):
   pointer = fg.boldblue('>')
   for choice in choices:
     print(choice)
-  print(cursor.hide()+cursor.up()*(len(choices))+pointer, end='                                               ')
+  print(cursor.hide()+cursor.up()*(len(choices))+pointer, end='   ')
   stdout.flush()
 
   pos = 1
@@ -22,11 +22,11 @@ def menu(choices):
     if (key == '\x1b[A' or key == 'w') and pos > 1:
       pos -= 1
       print(f'\r{text}\r' + cursor.up() + pointer + \
-            fg.bold(f'{""} {choices[pos-1]}'), end = '                                                        ')
+            fg.bold(f'{""} {choices[pos-1]}'), end = '      ')
     elif (key == '\x1b[B' or key == 's') and pos < len(choices):
       pos += 1
       print(f'\r{text}\r' + cursor.down() + pointer + \
-            fg.bold(f'{""} {choices[pos-1]}'), end = '                                                         ')
+            fg.bold(f'{""} {choices[pos-1]}'), end = '    ')
     elif key == '\n':
       print(cursor.down()*(len(choices)-pos)+cursor.show())
       return pos
@@ -95,6 +95,7 @@ if choice == 1:
       os.system("clear")
       print("You bring the form back")
     
+    
   elif choice == 2:
       print("she just tells you to bring over a form once you have decided")
       pause = input("")
@@ -113,4 +114,3 @@ elif choice == 3:
   pause = input("")
   print("Good question, What are you")
   choice = menu(['Im just traveling','Im not really anything', 'Im jobless', '*Stay Silent*'])
-
--- test.py
+++ test.py
@@ -7,26 +7,23 @@ from ansi import cursor
 def menu(choices):
   global pos
   pointer = fg.boldblue('>')
-  for choice in enumerate(choices):
-    text = f'  {choice[0]+1}. {choice[1]}'
-    if choice[0] == 0:
-      text = fg.bold(text)
-    print(text)
+  for choice in choices:
+    print(choice)
   print(cursor.hide()+cursor.up()*(len(choices))+pointer, end='')
   stdout.flush()
 
   pos = 1
   while 1:
     key = getkey()
-    text = f'  {pos}. {choices[pos-1]}'
+    text = f'{""} {choices[pos-1]}'
     if (key == '\x1b[A' or key == 'w') and pos > 1:
       pos -= 1
       print(f'\r{text}\r' + cursor.up() + pointer + \
-            fg.bold(f' {pos}. {choices[pos-1]}'), end = '')
+            fg.bold(f'{""} {choices[pos-1]}'), end = '')
     elif (key == '\x1b[B' or key == 's') and pos < len(choices):
       pos += 1
       print(f'\r{text}\r' + cursor.down() + pointer + \
-            fg.bold(f' {pos}. {choices[pos-1]}'), end = '')
+            fg.bold(f'{""} {choices[pos-1]}'), end = '                ')
     elif key == '\n':
       print(cursor.down()*(len(choices)-pos)+cursor.show())
       return pos
